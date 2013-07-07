2fc846b721741873dbfb1c3dd69b647deb994472
d9m <nigoro98@gmail.com>
2013-07-07 23:47:10 +0900

    add movie

--- 01/main.php
+++ 01/main.php
@@ -3,11 +3,15 @@ require_once(dirname(__FILE__)."/Movie.php");
 require_once(dirname(__FILE__)."/Rental.php");
 require_once(dirname(__FILE__)."/Customer.php");
 
-$movie = new Movie('Omoroi', 0);
-$rental = new Rental($movie, 7);
-$customer = new Customer('kuma');
+$movieOmoroi = new Movie('Omoroi', 0);
+$movieKanasi = new Movie('Kanasi', 0);
 
-$customer->addRental($rental);
+$rentalOmoroi = new Rental($movieOmoroi, 7);
+$rentalKanasi = new Rental($movieKanasi, 1);
+
+$customer = new Customer('kuma');
+$customer->addRental($rentalOmoroi);
+$customer->addRental($rentalKanasi);
 
 printf($customer->statement());
 
