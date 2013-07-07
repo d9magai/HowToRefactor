94851f2a35135664eb7395b7d3c17375f3877e43
d9m <nigoro98@gmail.com>
2013-07-07 23:50:14 +0900

    new release

--- 01/main.php
+++ 01/main.php
@@ -4,7 +4,7 @@ require_once(dirname(__FILE__)."/Rental.php");
 require_once(dirname(__FILE__)."/Customer.php");
 
 $movieOmoroi = new Movie('Omoroi', 0);
-$movieKanasi = new Movie('Kanasi', 0);
+$movieKanasi = new Movie('Kanasi', 1);
 
 $rentalOmoroi = new Rental($movieOmoroi, 7);
 $rentalKanasi = new Rental($movieKanasi, 1);
